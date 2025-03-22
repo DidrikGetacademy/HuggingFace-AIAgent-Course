6f1319e82c63eb39c68184992f118499b83aaa52
Didrik Nathaniel LLoyd Aasland Skjelbred <141577441+DidrikGetacademy@users.noreply.github.com>
2025-03-22 14:38:19 +0100

    update

---
+++ Unit-2 Frameworks for AiAgents/Inspecting_Agent_with_ OpenTelemetry_Langfuse.py
@@ -0,0 +1,4 @@
+#Inspecting Our Party Preparator Agent with OpenTelemetry and Langfuse 📡#
+#✅OpenTelemetry standard for instrumenting agent runs, allowing seamless inspection and logging.
+#✅With the help of Langfuse and the SmolagentsInstrumentor, Alfred can easily track and analyze his agent’s behavior.
+#Libaries---> pip install opentelemetry-sdk opentelemetry-exporter-otlp openinference-instrumentation-smolagents
